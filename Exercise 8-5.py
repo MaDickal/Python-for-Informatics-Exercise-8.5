182a3edafcf2f1d9fe96e851722c84e69f18ab8b
Matt Dickal <matt.dickal@gmail.com>
2015-05-26 18:41:49 -0700

    Exercise 8.5

---
+++ Exercise 8-5.py
@@ -0,0 +1,20 @@
+# prompts for file name
+fname = raw_input('Enter file name: ')
+
+# attempts to open the file
+try:
+	fhandle = open(fname)
+except:
+	print 'File cannot be opened:', fname
+	exit()
+
+count = 0
+
+for line in fhandle:
+# searches for the desired line and prints the second word
+	if line.startswith('From '):
+		line.split()
+		print line[1]
+		count = count + 1
+
+print 'There were', count, 'lines in the file with From as the first word'
